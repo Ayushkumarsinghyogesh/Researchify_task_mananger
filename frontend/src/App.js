ce24fcfd87398d0357f7633d42e6d7fa2a4a4e96
Ayushkumarsinghyogesh <ayushkumarsinghyogesh@gmail.com>
2024-08-01 01:26:19 +0530

    signup and login intergrate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,30 @@
-import React from 'react'
-import Home from './pages/Home'
+import React from 'react';
+import Home from './pages/Home';
 import AllTask from './pages/AllTask';
 import ImportantTask from './pages/ImportantTask';
 import CompletedTask from './pages/CompletedTask';
 import IncompletedTask from './pages/IncompletedTask';
-
-import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
-import signup from './pages/signup';
+import Signup from './pages/Signup'; 
+import Login from './pages/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-      <div className="bg-gray-900 text-white h-screen p-2 relative ">
-        <Router>
-          <Routes>
-            <Route exact path = '/' element={<Home/>} >
-              <Route index element={<AllTask/>} />
-              <Route path = '/importantTasks' element={<ImportantTask/>} />
-              <Route path = '/completedTasks' element={<CompletedTask/>} />
-              <Route path = '/incompleteTasks' element={<IncompletedTask/>} />
-            </Route >
-            <Route  path = "/signup" element = {signup}></Route>
-          </Routes>        
-        </Router>
-      </div>
-  )
+    <div className="bg-gray-900 text-white h-screen p-2 relative">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<AllTask />} />
+            <Route path="/importantTasks" element={<ImportantTask />} />
+            <Route path="/completedTasks" element={<CompletedTask />} />
+            <Route path="/incompleteTasks" element={<IncompletedTask />} />
+          </Route>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
